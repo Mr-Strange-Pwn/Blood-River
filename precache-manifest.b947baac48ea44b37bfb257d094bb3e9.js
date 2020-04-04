7a6bce04cf7cb432aac22a959c414ac1793b4952
Mr-Strange-Pwn <pawan.tiwari9411@gmail.com>
2020-04-03 18:22:51 -0700

    Updates

---
+++ precache-manifest.b947baac48ea44b37bfb257d094bb3e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aea6c10ed0c927728a5e72b101376cdb",
+    "url": "/Blood-River/index.html"
+  },
+  {
+    "revision": "96742814f6a494b41302",
+    "url": "/Blood-River/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "96742814f6a494b41302",
+    "url": "/Blood-River/static/js/2.582aae6e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Blood-River/static/js/2.582aae6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80ea5b63b8a777149cf9",
+    "url": "/Blood-River/static/js/main.d923e7ea.chunk.js"
+  },
+  {
+    "revision": "2658dbbb3b1148e03702",
+    "url": "/Blood-River/static/js/runtime-main.f6a580dd.js"
+  },
+  {
+    "revision": "fb98525f6dd10a4ed0d9e66402faf0ac",
+    "url": "/Blood-River/static/media/pwn.fb98525f.png"
+  }
+]);
